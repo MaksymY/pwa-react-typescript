f229e7c5de2c15ca45d74df698d863c640f11db5
MaksymY <maksym.yankivskyypro@gmail.com>
2020-06-12 01:03:59 +0200

    deam boy workbox is her

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,6 @@ import { ListPokemons } from "./components/ListPokemons";
 import { usePokemons } from "./hooks/pokemons.kook";
 
 function App() {
-  /* const [pokemons, setPokemon] = useState<Pokemon[]>([]);
-
-  useEffect(() => {
-    Promise.all(
-      Array.from({ length: 100 }, (_, index) =>
-        fetch(`https://pokeapi.co/api/v2/pokemon/${index + 1}`).then((value) =>
-          value.json(),
-        ),
-      ),
-    )
-      .then((values) => {
-        setPokemon(values);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []); */
   const pokemons = usePokemons();
 
   return (
